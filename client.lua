01281fd84a95107c393bce680a65046a79c5d16f
Snnaples <68153702+Snnaples@users.noreply.github.com>
2021-03-27 13:03:53 +0200

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,27 +1,13 @@
 vRP = Proxy.getInterface("vRP")
 
-Citizen.CreateThread(function()
-  while true do
-    Citizen.Wait(0)
-      DisableControlAction(0, 140, active)
-      DisableControlAction(0, 263, active)
-       DisableControlAction(0, 257, active)  
-       local ped = PlayerPedId()
-            	if IsPedArmed(ped, 6) then
-	    	DisableControlAction(1, 140, true)
-            	DisableControlAction(1, 141, true)
-            	DisableControlAction(1, 142, true)
-            	end
-    end
-    Citizen.Wait(0)
-end)
+
 
 function giveWeapon(hash)
     GiveWeaponToPed(GetPlayerPed(-1), GetHashKey(hash), 250, false, false)
 end
     
-RegisterCommand("giveweapon", function(source, args, rawCommand)
-    local user_id = vRP.getUserId(source)  
+RegisterCommand("giveweapon", function(args, rawCommand)
+ 
 local playerPed = GetPlayerPed(-1)
     local weaponHash = GetHashKey(args[1])
 	local ammoCount = 9999
@@ -29,9 +15,9 @@ local playerPed = GetPlayerPed(-1)
 	GiveWeaponToPed(playerPed, weaponHash, ammoCount, false)
 end)
 
-RegisterCommand('dacie', function(player)
+RegisterCommand('dacie', function()
     local car = GetHashKey("1310s")
-    local user_id = vRP.getUserId({player})
+
     RequestModel(car)
         while not HasModelLoaded(car) do
             RequestModel(car)
@@ -55,8 +41,8 @@ RegisterCommand("tinta", function(source)
 
    local ped = GetPlayerPed(-1)
     viata = (GetEntityHealth(ped)-0)
-    print(viata)
-    
+  
+   
     SetEntityHealth(ped, 0)
    Wait(1500)
     SetEntityHealth(ped, viata)
